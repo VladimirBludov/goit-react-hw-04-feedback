ad7edd04a40e47d702a66d290dbcd5d717125e5c
VladimirBludov <vladimir.bloodov@gmail.com>
2022-02-11 19:55:16 +0200

    Refactored to hooks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,59 +1,57 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import Buttons from '../Buttons';
 import Section from '../Section';
 import Statistics from '../Statistics';
 import s from './App.module.css';
 
-export default class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleUpdateStatistic = type => {
+    switch (type) {
+      case 'good':
+        setGood(state => state + 1);
+        break;
+      case 'neutral':
+        setNeutral(state => state + 1);
+        break;
+      case 'bad':
+        setBad(state => state + 1);
+        break;
+
+      default:
+        return;
+    }
   };
 
-  handleUpdateStatistic = type => {
-    this.setState(prevState => ({
-      [type]: prevState[type] + 1,
-    }));
+  const countTotalFeedback = () => good + neutral + bad;
+
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    return total ? Math.ceil((good * 100) / total) : 0;
   };
 
-  countTotalFeedback = () => {
-    const values = Object.values(this.state);
-    return values.reduce((total, value) => (total = total + value));
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    const total = this.countTotalFeedback();
-    const countPositiveFeedback = total
-      ? Math.ceil((this.state.good * 100) / total)
-      : 0;
-    return countPositiveFeedback;
-  };
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    const typesButton = Object.keys(this.state);
-
-    return (
-      <div className={s.wrapper}>
-        <div className={s.container}>
-          <Section title="Please leave feedback">
-            <Buttons
-              typesButton={typesButton}
-              updateStatistics={this.handleUpdateStatistic}
-            />
-          </Section>
-          <Section title="Statistics:">
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              countTotal={this.countTotalFeedback()}
-              positiveFeedback={this.countPositiveFeedbackPercentage()}
-            />
-          </Section>
-        </div>
+  return (
+    <div className={s.wrapper}>
+      <div className={s.container}>
+        <Section title="Please leave feedback">
+          <Buttons
+            typesButton={['good', 'neutral', 'bad']}
+            updateStatistics={handleUpdateStatistic}
+          />
+        </Section>
+        <Section title="Statistics:">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            countTotal={countTotalFeedback()}
+            positiveFeedback={countPositiveFeedbackPercentage()}
+          />
+        </Section>
       </div>
-    );
-  }
+    </div>
+  );
 }
